45ac2a69c1c98681c9677930d68bd38ce24a5f95
anikint <antiengo@gmail.com>
2017-08-28 16:34:31 +0200

    goToPosition now degree-sleep dependant

--- code/motor/head.py
+++ code/motor/head.py
@@ -24,7 +24,8 @@ class Head:
         #while self.BP.get_motor_encoder(self.PORT) not in range(position-10,position+10):
             oldValue = self.BP.get_motor_encoder(self.PORT)
             self.BP.set_motor_position(self.PORT, position)
-            time.sleep(max(0.2, degree/self.MAX_RANGE*4))
+            #time.sleep(max(0.2, degree/self.MAX_RANGE*4))
+            time.sleep(1)
             print (self.BP.get_motor_encoder(self.PORT))
         return
 
