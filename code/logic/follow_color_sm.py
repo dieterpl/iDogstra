e9175c36375473f23665f195cf2d5bfd0a5b0ca8
Lukas Karnowski <lukas.karnowski@gmail.com>
2017-08-28 12:39:59 +0200

    Use the bluetooth pipeline in follow_color_sm.py

--- code/logic/follow_color_sm.py
+++ code/logic/follow_color_sm.py
@@ -9,34 +9,53 @@ from sensors.bluetooth.bluetooth import BTDongle
 
 class FollowColorSM(StateMachine):
 
-    def __init__(self):
+    def __init__(self, btdongles):
+        """ btdongles is the list of bluetooth dongles"""
         StateMachine.__init__(self)
+
+        self.__btdongles = btdongles
 
         self._current_state.first_state = FollowState()
 
 
 class FollowState(State):
 
-    def __init__(self):
+    def __init__(self, btdongles):
+        """ btdongles is the list of bluetooth dongles"""
         State.__init__(self)
 
+        self.__btdongles = btdongles
+
+        # Create a pipeline that reads both camara and bluetooth inputs
+        # parallel and processes them sequentially
         self.__pipeline = \
-            create_sequential_pipeline([
-                lambda inp: camera.read(),
-                create_parallel_pipeline([
-                    create_sequential_pipeline([
-                        camera.ConvertColorspacePipeline(to='hsv'),
-                        camera.DetectColoredObjectPipeline(color='magenta')
+            create_parallel_pipeline([
+                # Camera inputs
+                create_sequential_pipeline([
+                    lambda inp: camera.read(),
+                    create_parallel_pipeline([
+                        create_sequential_pipeline([
+                            camera.ConvertColorspacePipeline(to='hsv'),
+                            camera.DetectColoredObjectPipeline(color='magenta')
+                        ]),
+                        camera.GetImageDimensionsPipeline()
                     ]),
-                    camera.GetImageDimensionsPipeline()
+                    camera.FindYDeviationPipeline()
                 ]),
-                camera.FindYDeviationPipeline()
+                # Bluetooth inputs
+                create_sequential_pipeline([
+                    ConstantPipeline(self.__btdongles),
+                    SnapshotBTDataPipeline(),
+                    RecommendedSpeedPipeline()
+                ])
             ])
 
         if DEBUG_MODE:
             def show_result(*_):
-                _, _, (bbox_ok, bbox) = self.__pipeline.steps[1].pipelines[0].step_results
-                _, (image_ok, image), _, (dev_ok, dev) = self.__pipeline.step_results
+                _, _, (bbox_ok,
+                       bbox) = self.__pipeline.steps[1].pipelines[0].step_results
+                _, (image_ok, image), _, (dev_ok,
+                                          dev) = self.__pipeline.step_results
 
                 # draw bounding box
                 if bbox_ok:
@@ -46,36 +65,25 @@ class FollowState(State):
 
                 # add deviation as text
                 if dev_ok:
-                    cv2.putText(image, str(dev), (0, image.shape[0] - 5), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, .6, [0, 255, 0])
+                    cv2.putText(image, str(
+                        dev), (0, image.shape[0] - 5), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, .6, [0, 255, 0])
 
                 cv2.imshow('camtest', image)
                 cv2.waitKey(1)
 
             self.__pipeline.execute_callbacks = [show_result]
 
-        # todo port to pipelines
-        uuid = '6951e12f049945d2930e1fc462c721c8'
-        self.btdongles = [BTDongle(i, uuid) for i in range(2)]
-        for dongle in self.btdongles:
-            dongle.start()
-
-        self.current_speed = 0
-
     @property
     def pipeline(self):
         return self.__pipeline
 
     def on_update(self, hist):
-        dev_ok, dev = hist[-1]
+        speed_result, dev_result = hist[-1]
 
-        _sum = sum(dongle.snapshot_data().avg() for dongle in self.btdongles)
-        _avg = _sum / len(self.btdongles)
-        if _avg < 65:
-            speed = 0
-        else:
-            speed = round(min(100.0, 15 + 4 * (_avg - 65)))
+        speed_ok, speed = speed_result
+        dev_ok, dev = dev_result
 
-        if dev_ok:
+        if dev_ok and speed_ok:
             if dev < -0.6:
                 movement.right(50)
             elif dev < -0.3:
@@ -89,10 +97,8 @@ class FollowState(State):
             elif dev > 0.2:
                 movement.left(10)
             else:
-                self.current_speed = speed
                 movement.forward(speed)
         else:
-            self.current_speed = max(0, self.current_speed - 1)
             movement.stop()
 
         return self
@@ -100,10 +106,15 @@ class FollowState(State):
     def on_exit(self):
         movement.stop()
 
+
 if __name__ == '__main__':
     if DEBUG_MODE:
         cv2.namedWindow('camtest')
 
-    FollowColorSM().run()
+    # Init bluetooth dongles
+    uuid = "6951e12f049945d2930e1fc462c721c8"
+    btdongles = [BTDongle(i, uuid) for i in range(2)]
+    for dongle in self.btdongles:
+        dongle.start()
 
-
+    FollowColorSM(btdongles).run()
