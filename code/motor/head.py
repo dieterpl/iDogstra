b359d86b6721a0c355a0435cedfdc26ff7fde86f
anikint <antiengo@gmail.com>
2017-08-28 15:07:15 +0200

    offset removed, optimization

--- code/motor/head.py
+++ code/motor/head.py
@@ -20,6 +20,7 @@ class Head:
         while self.BP.get_motor_encoder != oldValue:
         #while self.BP.get_motor_encoder(self.PORT) not in range(position-10,position+10):
             self.BP.set_motor_position(self.PORT, position)
+            oldValue = self.BP.get_motor_encoder(self.PORT)
             print (self.BP.get_motor_encoder(self.PORT))
         return
 
