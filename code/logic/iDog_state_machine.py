a954017dab752d31727fa27ff66229c6f53b4207
Paul Dieterich <dieterpl@studi.informatik.uni-stuttgart.de>
2017-08-29 12:46:55 +0200

    implemented states and testing on pi

--- code/logic/iDog_state_machine.py
+++ code/logic/iDog_state_machine.py
@@ -1,13 +1,13 @@
 
 from logic.statemachine import *
-from logic import states
+from logic.states import *
 
 
 class IDog(StateMachine):
 
     def __init__(self):
         StateMachine.__init__(self)
-        self._current_state.first_state = states.SearchState()
+        self._current_state.first_state = SearchState()
 
 
 
