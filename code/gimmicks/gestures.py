6aa4c8c1f63b206f30cee6ac321149c8cdb3c66e
anikint <antiengo@gmail.com>
2017-08-29 10:36:02 +0200

    new code error fix

--- code/gimmicks/gestures.py
+++ code/gimmicks/gestures.py
@@ -13,7 +13,7 @@ dont use this, TODO: implement this, currently bei head.py
 class Gestures:
     def __enter__(self):
         self.head = head.Head()
-        self.screen = screenDog.Gesture()
+        self.screen = screenDog.ScreenDog()
         return self
 
 
