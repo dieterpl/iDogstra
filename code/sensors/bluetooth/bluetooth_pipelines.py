4c756e9a7d7486fc9ad5f98d29df92b98f0c318a
Lukas Karnowski <lukas.karnowski@gmail.com>
2017-08-30 10:04:51 +0200

    Move commonly used bluetooth pipelines into own file

---
+++ code/sensors/bluetooth/bluetooth_pipelines.py
@@ -0,0 +1,17 @@
+from sensors.bluetooth import bluetooth
+from sensors import pipeline
+
+
+def recommended_speed_pipeline():
+    return pipeline.PipelineSequence(
+        pipeline.ConstantPipeline(config.BT_DONGLES),
+        bluetooth.SnapshotBTDataPipeline(),
+        bluetooth.RecommendedSpeedPipeline()
+    )
+
+def user_distance_estimation_pipeline():
+    return pipeline.PipelineSequence(
+        pipeline.ConstantPipeline(config.BT_DONGLES),
+        bluetooth.SnapshotBTDataPipeline(),
+        bluetooth.UserDistanceEstimationpipeline()
+    )
