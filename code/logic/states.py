830ad6515807bcd85b2d9146a0eb69ea9381edc1
Paul Dieterich <dieterpl@studi.informatik.uni-stuttgart.de>
2017-08-30 09:54:47 +0200

    fixing logic and testing on pi

--- code/logic/states.py
+++ code/logic/states.py
@@ -158,9 +158,10 @@ class TrackState(State):
     def on_update(self, hist):
         pipeline_result = hist[-1]
         logging.debug("TrackState Pipeline results {}".format(hist[-1]))
-        logging.debug("Values {}{}{}".format(cam_ok, bt_ok))
+
         # unpack results
         cam_ok, bt_ok = self.pipeline[0].success_state, self.pipeline[1].success_state
+        logging.debug("Values {}{}{}".format(cam_ok, bt_ok))
         dev, distance = pipeline_result
         # if there are no result values go to wait state
         if not cam_ok and not bt_ok:
