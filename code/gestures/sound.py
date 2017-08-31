32c55e294d3ef79402a31e453793f09e0f1fd951
Jan <jan.rollmann94@googlemail.com>
2017-08-31 12:19:55 +0200

    Wrap sound to play in try except block

--- code/gestures/sound.py
+++ code/gestures/sound.py
@@ -2,13 +2,17 @@ import pygame
 import os
 from config import config
 
-class Sound()
+
+class Sound:
 
     def __init__(self):
         pygame.mixer.init()
 
     def bark(self):
-        pygame.mixer.music.load(os.path.join(config.SOUNDPATH, "bark.wav"))
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy() == True:
-            continue
+        try:
+            pygame.mixer.music.load(os.path.join(config.SOUNDPATH, "bark.wav"))
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                continue
+        except KeyboardInterrupt:
+            print('exit')
