198beb17c73e4fa92057ab4ce92d987c45f4eeda
anikint <antiengo@gmail.com>
2017-08-31 12:15:48 +0200

    sound bark

--- code/gestures/sound.py
+++ code/gestures/sound.py
@@ -2,8 +2,13 @@ import pygame
 import os
 from config import config
 
-pygame.mixer.init()
-pygame.mixer.music.load(os.path.join(config.SOUNDPATH, "bark.wav"))
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
+class Sound()
+
+    def __init__(self):
+        pygame.mixer.init()
+
+    def bark(self):
+        pygame.mixer.music.load(os.path.join(config.SOUNDPATH, "bark.wav"))
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
