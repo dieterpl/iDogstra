cb562e4f500dce75b0e1b97347c26a241a9b32b4
Lukas Karnowski <lukas.karnowski@gmail.com>
2017-08-31 09:20:37 +0200

    Move bt dongle list into iDog_sm.py

--- code/logic/iDog_sm.py
+++ code/logic/iDog_sm.py
@@ -14,6 +14,7 @@ from scipy.interpolate import interp1d
 
 __ultrasonic = None
 __infrared = None
+__bt_dongles = None
 
 class IDog(StateMachine):
 
@@ -29,9 +30,10 @@ class IDog(StateMachine):
         __infrared.start_thread()
 
         logging.debug("Starting BT-Dongles")
-        config.BT_DONGLES = [bluetooth.BTDongle(i, config.BT_TARGET_UUID)
+        global __bt_dongles
+        __bt_dongles = [bluetooth.BTDongle(i, config.BT_TARGET_UUID)
                              for i in config.BT_DONGLE_IDS]
-        for dongle in config.BT_DONGLES:
+        for dongle in __bt_dongles:
             dongle.start()
 
         self._current_state.first_state = SearchState()
